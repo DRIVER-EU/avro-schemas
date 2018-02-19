b15e76b9932d115d5d4eecec94ae7c06cb110c14
Erik Vullings <erik.vullings@gmail.com>
2018-02-19 18:39:08 +0100

    Removed dist folder.

--- dist/parser.d.ts
+++
@@ -1,9 +0,0 @@
-import { ICommandOptions } from './cli';
-export declare class Parser {
-    constructor(option: ICommandOptions);
-    private validateJSON(json, schema);
-    private inferSchema(json, schemaFile);
-    private loadFile(filename);
-    private loadJSON(filename);
-    private loadXML(filename);
-}
